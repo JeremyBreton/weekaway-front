a71b39c700976f89b347aab22186c5e5fe3cf859
JeremyBreton <jeremy.breton@oclock.school>
2023-10-24 18:37:57 +0200

    :construction: installation MUI + test header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,11 @@
-import logo from '../../assets/logo.svg';
-
+import Navbar from '../Navbar/Navbar';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: '#004643' }}>
+      <h1>Weekaway</h1>
+      <Navbar />
     </div>
   );
 }
