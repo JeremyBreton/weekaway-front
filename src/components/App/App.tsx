5117204c0d82dcbdc35d33538f5e1ee6b7995d08
caldora enzo <ecaldora@gmail.com>
2023-10-24 21:33:12 +0200

    :work in progress: navbar responsive and first visual of the goal of our site

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from '../Navbar/Navbar';
 import './App.scss';
+import Test from './Test/Test';
 
 function App() {
   return (
     <div style={{ backgroundColor: '#004643' }}>
       <h1>Weekaway</h1>
       <Navbar />
+      <Test />
     </div>
   );
 }
